b3af76f6dde62e1b0194bb9c7bcd43f6f781721f
Mitchel Pigsley <mpigsley@gmail.com>
2024-01-13 17:54:44 -0700

    Images babyyyy

--- src/pages/{markdownRemark.fields__slug}.tsx
+++ src/pages/{markdownRemark.fields__slug}.tsx
@@ -1,47 +1,67 @@
 import * as React from 'react';
 import { type PageProps, graphql, Link, type HeadFC } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 
 export default function SpeciesProfileTemplate({
   data,
 }: Readonly<PageProps<Queries.SpeciesProfileQuery>>): JSX.Element {
+  console.log(data);
   return (
-    <main className="container page">
-      <section className="row">
-        <Link to="/">&lt; Back to Home</Link>
-        <h3 className="noMargin">{data.markdownRemark?.frontmatter?.name}</h3>
-        <h5>{data.markdownRemark?.frontmatter?.scientific_name}</h5>
-        <p>
-          <i>{data.markdownRemark?.frontmatter?.taxonomy?.join(' > ')}</i>
-        </p>
-        <hr />
-      </section>
-      <section className="row">
-        <div
-          className="eight columns"
-          dangerouslySetInnerHTML={{ __html: data.markdownRemark?.html ?? '' }}
-        />
-        <div className="four columns">
-          <b>External Links</b>
-          <ul>
-            {data.markdownRemark?.frontmatter?.external_links?.map((item) => (
-              <li key={item?.link}>
-                <a href={item?.link ?? '/'} target="_blank" rel="noreferrer">
-                  {item?.tag}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <b>Tags</b>
-          <ul>
-            {data.markdownRemark?.frontmatter?.tags?.map((item) => (
-              <li key={item}>
-                <i>{item}</i>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </section>
-    </main>
+    <>
+      <div className="horizontalScroll u-full-width">
+        {data.markdownRemark?.frontmatter?.photos?.map((item) => {
+          if (item?.childImageSharp?.gatsbyImageData) {
+            return (
+              <GatsbyImage
+                className="horizontalScrollItem"
+                key={item?.childImageSharp?.id}
+                image={item?.childImageSharp?.gatsbyImageData}
+                alt={`${data.markdownRemark?.frontmatter?.name} (${data.markdownRemark?.frontmatter?.scientific_name})`}
+              />
+            );
+          }
+        })}
+      </div>
+      <main className="container page">
+        <section className="row">
+          <Link to="/">&lt; Back to Home</Link>
+          <h3 className="noMargin">{data.markdownRemark?.frontmatter?.name}</h3>
+          <h5>{data.markdownRemark?.frontmatter?.scientific_name}</h5>
+          <p>
+            <i>{data.markdownRemark?.frontmatter?.taxonomy?.join(' > ')}</i>
+          </p>
+          <hr />
+        </section>
+        <section className="row">
+          <div
+            className="eight columns"
+            dangerouslySetInnerHTML={{
+              __html: data.markdownRemark?.html ?? '',
+            }}
+          />
+          <div className="four columns">
+            <b>External Links</b>
+            <ul>
+              {data.markdownRemark?.frontmatter?.external_links?.map((item) => (
+                <li key={item?.link}>
+                  <a href={item?.link ?? '/'} target="_blank" rel="noreferrer">
+                    {item?.tag}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <b>Tags</b>
+            <ul>
+              {data.markdownRemark?.frontmatter?.tags?.map((item) => (
+                <li key={item}>
+                  <i>{item}</i>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
@@ -65,6 +85,12 @@ export const pageQuery = graphql`
         external_links {
           link
           tag
+        }
+        photos {
+          childImageSharp {
+            id
+            gatsbyImageData(height: 500, quality: 90, layout: CONSTRAINED)
+          }
         }
       }
     }
