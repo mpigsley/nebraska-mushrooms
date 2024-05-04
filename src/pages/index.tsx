6586b337346be3e87f1097ea2e8e5a6c38aa3fe8
Mitchel Pigsley <mpigsley@gmail.com>
2024-05-04 09:24:15 -0600

    Pull in static images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { graphql, PageProps, type HeadFC } from 'gatsby';
+import { graphql, PageProps } from 'gatsby';
 import * as React from 'react';
 
-import PageLayout from '../components/PageLayout';
+import WildMushroomEdibility from '../img/wild-mushroom-edibility.jpeg';
+import HowToUseThisWebiste from '../img/how-to-use-this-website.jpeg';
+import IndianCaveStatePark from '../img/indian-cave-state-park.jpeg';
+import IdentificationKey from '../img/identification-key.jpeg';
+import MushrooomList from '../img/mushroom-list.jpeg';
 import { Tag, getTagClass } from '../utils/tag.util';
+import PageLayout from '../components/PageLayout';
+
+console.log(IndianCaveStatePark);
 
 export default function IndexPage({
   data,
 }: Readonly<PageProps<Queries.LocationIndexQuery>>): JSX.Element {
   const [rando, setRando] = React.useState<any>(null);
 
-  const generateRando = () => setRando(data.species.edges[Math.floor(Math.random() * (data.species.edges.length - 1))].node);
+  const generateRando = () =>
+    setRando(
+      data.species.edges[
+        Math.floor(Math.random() * (data.species.edges.length - 1))
+      ].node,
+    );
 
   React.useEffect(() => {
     generateRando();
@@ -19,93 +31,96 @@ export default function IndexPage({
   return (
     <PageLayout>
       <div>
-        <div>
-          <h4>Quick Links</h4>
-          <div className="qlink-grid">
-            <div className="qlink-tile qlink-tile-1">
-              <div className="qlink-tile-title">
-                <a href='/articles/concerning-wild-mushroom-edibility'>
-                  <h5>Wild Mushroom Edibility</h5>
-                </a>
-              </div>
-            </div>
-            <div className="qlink-tile qlink-tile-2">
-              <div className="qlink-tile-title">
-                <a href='/articles/manual'>
-                  <h5>How to Use This Website</h5>
-                </a>
-              </div>
-            </div>
-            <div className="qlink-tile qlink-tile-3">
-              <div className="qlink-tile-title">
-                <a href='/articles/key'>
-                  <h5>Identification Key</h5>
-                </a>
-              </div>
-            </div>
-            <div className="qlink-tile qlink-tile-4">
-              <div className="qlink-tile-title">
-                <a href='/location/indian-cave-state-park/'>
-                  <h5>Mushroom List</h5>
-                </a>
-              </div>
-            </div>
+        <h4>Quick Links</h4>
+        <div className="row mb-4">
+          <div className="six columns relative link-tile content-centered">
+            <img src={WildMushroomEdibility} className="centered-image" />
+            <a
+              className="grid-link"
+              href="/articles/concerning-wild-mushroom-edibility"
+            >
+              <h5 className="noMargin">Wild Mushroom Edibility</h5>
+            </a>
+          </div>
+          <div className="six columns relative link-tile content-centered">
+            <img src={HowToUseThisWebiste} className="centered-image" />
+            <a className="grid-link" href="/articles/nanual">
+              <h5 className="noMargin">How to Use This Website</h5>
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="six columns relative link-tile content-centered">
+            <img src={IdentificationKey} className="centered-image" />
+            <a className="grid-link" href="/articles/key">
+              <h5 className="noMargin">Identification Key</h5>
+            </a>
+          </div>
+          <div className="six columns relative link-tile content-centered">
+            <img src={MushrooomList} className="centered-image" />
+            <a className="grid-link" href="/location/indian-cave-state-park">
+              <h5 className="noMargin">Mushroom List</h5>
+            </a>
           </div>
         </div>
         <hr />
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <h4>Random Mushroom</h4> <button onClick={generateRando}>Regenerate</button>
+        <div className="content-spaced">
+          <h4>Random Mushroom</h4>
+          <button onClick={generateRando}>Regenerate</button>
         </div>
 
-
-        <div>
-          {!!rando?.frontmatter &&
-            (
-              <div className="qlink-grid">
-                <GatsbyImage
-                  className="randoImage qlink-tile"
-                  key={rando.frontmatter.photos[0]?.childImageSharp?.id}
-                  image={rando.frontmatter.photos[0]?.childImageSharp?.gatsbyImageData}
-                  alt={`${rando.frontmatter.name} (${rando.frontmatter.photos[0]?.scientific_name})`}
-                />
-                <div>
-                  <h5><a href={rando.fields.slug}>{rando?.frontmatter.name}</a></h5>
-                  <h6><a href={rando.fields.slug}>{rando?.frontmatter.scientific_name}</a></h6>
-                  <p>
-                    {rando?.frontmatter.html}
-                  </p>
-                  {!!rando?.frontmatter?.tags?.length && (
-                    <>
-                      <ul>
-                        {[...rando.frontmatter.tags]
-                          .sort((a, b) => (a && b ? a.localeCompare(b) : 0))
-                          .map((item) => (
-                            <li key={item}>
-                              <span className={`${getTagClass(item as Tag)} tag`}>
-                                {item}
-                              </span>
-                            </li>
-                          ))}
-                      </ul>
-                    </>
-                  )}
-                </div>
+        <div className="row">
+          {!!rando?.frontmatter && (
+            <>
+              <GatsbyImage
+                className="six columns"
+                key={rando.frontmatter.photos[0]?.childImageSharp?.id}
+                image={
+                  rando.frontmatter.photos[0]?.childImageSharp?.gatsbyImageData
+                }
+                alt={`${rando.frontmatter.name} (${rando.frontmatter.photos[0]?.scientific_name})`}
+              />
+              <div className="six columns my-3">
+                <h5 className="noMargin">
+                  <a href={rando.fields.slug}>
+                    {rando?.frontmatter.name ??
+                      rando?.frontmatter.scientific_name}
+                  </a>
+                </h5>
+                {!!rando?.frontmatter.name && (
+                  <h6>{rando?.frontmatter.scientific_name}</h6>
+                )}
+                <p>{rando?.frontmatter.html}</p>
+                {!!rando?.frontmatter?.tags?.length && (
+                  <>
+                    <ul>
+                      {[...rando.frontmatter.tags]
+                        .sort((a, b) => (a && b ? a.localeCompare(b) : 0))
+                        .map((item) => (
+                          <li key={item}>
+                            <span className={`${getTagClass(item as Tag)} tag`}>
+                              {item}
+                            </span>
+                          </li>
+                        ))}
+                    </ul>
+                  </>
+                )}
               </div>
-            )}
+            </>
+          )}
         </div>
       </div>
       <hr />
       <div>
         <h4>Locations</h4>
-        <div className='location-grid'>
-          <div className='qlink-tile qlink-tile-5'>
-            <div className="qlink-tile-title">
-              <a href='/location/indian-cave-state-park/'>
-                <h5>Indian Cave State Park</h5>
-              </a>
-            </div>
+        <div className="row">
+          <div className="twelve columns relative location-tile content-centered">
+            <img src={IndianCaveStatePark} className="centered-image" />
+            <a className="grid-link" href="/location/indian-cave-state-park/">
+              <h5 className="noMargin">Indian Cave State Park</h5>
+            </a>
           </div>
-          {/* <div className='grid-item'><a href='/location/indian-cave-state-park/'>TODO Entire State</a></div> */}
         </div>
       </div>
     </PageLayout>
@@ -142,6 +157,8 @@ export const query = graphql`
                 id
                 gatsbyImageData(
                   quality: 90
+                  height: 300
+                  width: 468
                   layout: CONSTRAINED
                 )
               }
