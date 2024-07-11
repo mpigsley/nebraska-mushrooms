1659d7e54a81d7607045df33365deb873a3b15bd
Derek Zeller <root.dzeller@gmail.com>
2024-07-11 14:30:19 -0500

    better var name

--- src/templates/species.tsx
+++ src/templates/species.tsx
@@ -53,11 +53,11 @@ export default function SpeciesProfileTemplate({
           <h3 className="noMargin">{commonName || scientificName}</h3>
           {commonName && <h5>{scientificName}</h5>}
           <p>
-            {data.species?.frontmatter?.taxonomy?.map((val, index: number) => (
+            {data.species?.frontmatter?.taxonomy?.map((taxa, index: number) => (
               <i>
               {index > 0 && (<> &gt; </>)}
-              <a href={`http://www.inaturalist.org/taxa/search?q=${val?.toLowerCase().replaceAll(' ', '+')}`}>
-                {val}
+              <a href={`http://www.inaturalist.org/taxa/search?q=${taxa?.toLowerCase().replaceAll(' ', '+')}`}>
+                {taxa}
               </a>
               </i>
             ))}
