755f181f6d728de7b782878b0c1f2154bea7453f
Mitchel Pigsley <mpigsley@gmail.com>
2025-02-01 11:06:06 -0700

    New favicon... idk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 
+import Favicon from '../img/favicon.svg';
+import { Link } from 'gatsby';
+
 export default function Header() {
   return (
     <header className="header">
-      <div className="container row">
-        <a href="/" className="header-title-link">
-          <h5 className="noMargin">Mushrooms of Nebraska</h5>
-        </a>
-        <p className="noMargin">
-          Important! Please read our{' '}
-          <a href="/articles/concerning-wild-mushroom-edibility/">
-            disclaimer on edibility.
+      <div className="container row flex items-center">
+        <Link to="/">
+          <img src={Favicon} className="logo action-button" />
+        </Link>
+        <div>
+          <a href="/" className="header-title-link">
+            <h5 className="noMargin">Mushrooms of Nebraska</h5>
           </a>
-        </p>
+          <p className="noMargin">
+            Important! Please read our{' '}
+            <a href="/articles/concerning-wild-mushroom-edibility/">
+              disclaimer on edibility.
+            </a>
+          </p>
+        </div>
       </div>
     </header>
   );
